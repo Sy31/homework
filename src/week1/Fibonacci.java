b9f857d8910f2e04345dda084bd34e99adb0a1fe
Sy31 <45235423+Sy31@users.noreply.github.com>
2018-11-29 21:30:51 +0800

    Update Fibonacci.java

--- src/week1/Fibonacci.java
+++ src/week1/Fibonacci.java
@@ -26,6 +26,17 @@ public class Fibonacci {
 	 * @return
 	 */
 	public static int calculateIteratively(int n) {
-		return 0;
+		if (n==1){
+			return 0;
+		}else if(n==2){
+			return 1;
+		}else{
+			int a=0,b=1,c=0;
+			for(i=3,i<=n,i++){
+			c=a+b;
+			a=b;
+			b=c;
+			}
+			return c
 	}
 }
