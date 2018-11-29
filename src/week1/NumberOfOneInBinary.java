6cd3b98b5233cc89a3eea851318ac32810ff227f
Sy31 <45235423+Sy31@users.noreply.github.com>
2018-11-29 22:19:32 +0800

    Update NumberOfOneInBinary.java

--- src/week1/NumberOfOneInBinary.java
+++ src/week1/NumberOfOneInBinary.java
@@ -8,15 +8,33 @@ public class NumberOfOneInBinary {
 	 * @return
 	 */
 	public static int numberOfOneRecursively(int n) {
-		return 0;
-	}
+		if (n==0){
+			return 0;
+		}else{
+			if(n%2==0){
+				return n/2;
+			}
+			else if(n%2==1){
+				return n/2+1;
+			}
+			
 	
+			
+			
 	/**
 	 * 循环的方式计算二进制数中一共有几个1
 	 * @param n
 	 * @return
 	 */
 	public static int numberOfOneIteratively(int n) {
-		return 0;
+		if(n==0){
+			return 0;
+		}else{
+			int count=0;
+		 	while(n!=0){
+			count++;
+			n=n&(n-1);
+		}
+		
 	}
 }
